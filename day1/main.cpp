cfb0afa64a5938f0eb3fae04e28ca97c85e775bd
pioteeer <163779476+R33etro@users.noreply.github.com>
2024-12-02 13:10:49 +0100

    Update main.cpp

--- day1/main.cpp
+++ day1/main.cpp
@@ -9,9 +9,11 @@ int main() {
 
     std::fstream input_file;
     input_file.open("C:/Users/DELL/Desktop/advent_of_code/day1/day1.txt");
-    int a, b, sum = 0;
+
+    int a, b, sum = 0, similarity_score = 0;
     std::pmr::vector<int> tab1;
     std::pmr::vector<int> tab2;
+
     std::string line;
     while (std::getline(input_file, line)) {
         std::stringstream ss(line);
@@ -19,12 +21,27 @@ int main() {
         tab1.push_back(a);
         tab2.push_back(b);
     }
+
+    //part 1
     std::sort(tab1.begin(), tab1.end());
     std::sort(tab2.begin(), tab2.end());
-
     for (int i = 0; i < tab1.size(); i++) {
         sum += abs(tab1[i] - tab2[i]);
     }
+
+    //part 2
+    for (int i = 0; i < tab1.size(); i++) {
+        int counter = 0;
+        for (int j = 0; j < tab2.size(); j++) {
+            if (tab1[i] == tab2[j]) {
+                counter++;
+            }
+        }
+        similarity_score += tab1[i]*counter;
+    }
+
     input_file.close();
-    return sum; //1651298 - correct
+    std::cout << sum << std::endl; //sum = 1651298 - correct
+    std::cout << similarity_score << std::endl; //sim_score = 21306195 - correct
+    return 0;
 }
